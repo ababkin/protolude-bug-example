2f9dcf6be72efa93157d85a322aa962ca9c8f498
Alex Babkin <ababkin@gmail.com>
2019-02-07 11:39:27 -0500

    make even smaller

--- app/Main.hs
+++ app/Main.hs
@@ -1,29 +1,33 @@
 {-# LANGUAGE FlexibleContexts      #-}
 {-# LANGUAGE FlexibleInstances     #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedStrings     #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
 
 module Main where
 
-import           Lib     ()
--- import           Lib       (Foo (..))
+import           Lib       ()
+-- import           Lib     (Foo (..))
 
--- import           Conv (toS)
+import qualified Data.Text as T
 import           Prelude
 
+-- taken and simplified from the Protolude package:
+-- https://github.com/sdiehl/protolude/blob/master/src/Protolude/Conv.hs
 class StringConv a b where
   strConv :: a -> b
 
 toS :: StringConv a b => a -> b
 toS = strConv
 
+instance StringConv T.Text String where strConv = T.unpack
+
 
 main :: IO ()
 main = do
-  loop "blah"
+  loop ("blah" :: T.Text)
 
   where
-    loop foo = do
-      pure $ Foo (toS foo)
-      pure ()
+    loop foo =
+      print $ Foo (toS foo)
